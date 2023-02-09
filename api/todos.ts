faf5597fa9315e08335fa3567178d0c429edee67
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-02-10 01:32:55 +0500

    test work mongodb

--- api/todos.ts
+++ api/todos.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import Document from "../schemas/test";
 const router = express.Router();
 
 
 
-router.get('/', async (req, res) => {
+router.get('/todos', async (req, res) => {
     try {
+        const newDocument = new Document({
+            field: 'field1',
+            field2: 2
+        });
+
+        await newDocument.save();
+
         res.json({
             status: 200,
             message: "Работает блять!"
