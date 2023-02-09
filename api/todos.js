ef52cdd3f67505b2be7d5aa87cc88f413b7618dd
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-02-09 16:24:43 +0500

    test commit

--- api/todos.js
+++ api/todos.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         res.json({
             status: 200,
             message: "Работает блять!"
         });
-    } catch (e) {
+    }
+    catch (e) {
         console.error(e);
         return res.status(500).send('Server error');
     }
-});
-
-module.exports = router;
+}));
+exports.default = router;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const todos = require('./api/todos');
-const app = express();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./api/todos"));
+const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
-
-app.use('/api/todos', todos);
-
+app.use('/api/todos', todos_1.default);
 app.listen(PORT, () => console.log(`Server is running in PORT: ${PORT}`));
